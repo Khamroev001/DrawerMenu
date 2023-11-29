016f51097ab976829733b47cde1163af28c3a6b6
Khamroev001 <sattorkhamroev@gmail.com>
2023-11-29 09:53:19 +0500

    completed

--- app/src/main/java/com/example/drawermenu/MainActivity.kt
+++ app/src/main/java/com/example/drawermenu/MainActivity.kt
@@ -21,9 +21,14 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -38,231 +43,261 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.drawermenu.ui.theme.DrawerMenuTheme
-
+import kotlinx.coroutines.launch
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeCount: Int? = null
+)
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DrawerMenuTheme {
-                // A surface container using the 'background' color from the theme
+            DrawerMenuTheme() {
+                val items = listOf(
+                    NavigationItem(
+                        title = "All",
+                        selectedIcon = Icons.Default.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                    ),
+                    NavigationItem(
+                        title = "Profile",
+                        selectedIcon = Icons.Default.Person,
+                        unselectedIcon = Icons.Outlined.Person,
+                    ),
+                    NavigationItem(
+                        title = "Urgent",
+                        selectedIcon = Icons.Default.Info,
+                        unselectedIcon = Icons.Outlined.Info,
+                        badgeCount = 45
+                    ),
+                    NavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Default.Settings,
+                        unselectedIcon = Icons.Outlined.Settings,
+                    )
+                )
+
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                        AppNavigation()
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+                    var selectedItemIndex by rememberSaveable {
+                        mutableStateOf(0)
+                    }
+                    ModalNavigationDrawer(
+                        drawerContent = {
+                            ModalDrawerSheet {
+
+                                DrawerHeader()
+
+                                Spacer(modifier = Modifier.height(16.dp))
+                                items.forEachIndexed { index, item ->
+                                    NavigationDrawerItem(
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        selected = index == selectedItemIndex,
+                                        onClick = {
+                                            navController.navigate(item.title)
+                                            selectedItemIndex = index
+                                            scope.launch {
+                                                drawerState.close()
+                                            }
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex) {
+                                                    item.selectedIcon
+                                                } else item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
+                                        },
+                                        badge = {
+                                            item.badgeCount?.let {
+                                                Text(text = item.badgeCount.toString())
+                                            }
+                                        },
+                                        modifier = Modifier
+                                            .padding(NavigationDrawerItemDefaults.ItemPadding)
+                                    )
+                                }
+                            }
+                        },
+                        drawerState = drawerState
+                    ) {
+                        Scaffold(
+                            topBar = {
+                                TopAppBar(
+                                    title = {
+                                        Text(text = "Drawer")
+
+                                    },
+                                    navigationIcon = {
+                                        IconButton(onClick = {
+                                            scope.launch {
+                                                drawerState.open()
+                                            }
+                                        }) {
+                                            Icon(
+                                                imageVector = Icons.Default.Menu,
+                                                contentDescription = "Menu"
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                        ) {
+                            NavHost(
+                                navController = navController,
+                                startDestination = "All"
+                            ) {
+                                composable("All") {
+                                    HomeScreen()
+                                }
+                                composable("Profile") {
+                                    ProfileScreen()
+                                }
+                                composable("Urgent") {
+                                    UrgentScreen()
+                                }
+                                composable("Settings") {
+                                    SettingsScreen()
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+}
 
-
-
-
-
-
-
-    @Composable
-    fun DrawerHeader(
-        modifier: Modifier = Modifier
+@Composable
+fun DrawerHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(60.dp)
-                    .clip(CircleShape)
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text(
+                text = "Sattor Khamroev",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
             )
-
-            Spacer(modifier = Modifier.width(16.dp))
-
-            Column {
-                Text(
-                    text = "John Doe",
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.onPrimary
-                )
-                Text(
-                    text = "john.doe@example.com",
-                    fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colorScheme.onPrimary
-                )
-            }
+            Text(
+                text = "sattorkhamroev@gmail.com",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+            )
         }
     }
+}
 
-    @Composable
-    fun DrawerContent(
-        navController: NavController
+
+
+
+
+
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.primary)
-        ) {
-            DrawerHeader(modifier = Modifier.padding(16.dp))
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            DrawerItem(
-                text = "Home",
-                icon = Icons.Default.Home,
-                onClick = { navController.navigate("home") }
-            )
-
-            DrawerItem(
-                text = "Profile",
-                icon = Icons.Default.Person,
-                onClick = { navController.navigate("profile") }
-            )
-
-            DrawerItem(
-                text = "Settings",
-                icon = Icons.Default.Settings,
-                onClick = { navController.navigate("settings") }
-            )
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            DrawerItem(
-                text = "Logout",
-                icon = Icons.Default.ExitToApp,
-                onClick = {
-                    // Implement logout logic here
-                }
-            )
-        }
+        Text(
+            text = "Home Screen",
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
+}
 
-    @Composable
-    fun DrawerItem(
-        text: String,
-        icon: ImageVector,
-        onClick: () -> Unit
+@Composable
+fun ProfileScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable(onClick = onClick)
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(imageVector = icon, contentDescription = null, modifier = Modifier.size(24.dp))
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(text = text, color = MaterialTheme.colorScheme.onPrimary)
-        }
+        Text(
+            text = "Profile Screen",
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
+}
 
-
-
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter",
-        "UnusedMaterial3ScaffoldPaddingParameter"
-    )
-    @Composable
-    fun AppNavigation() {
-        val navController = rememberNavController()
-        val drawerState = rememberDrawerState(DrawerValue.Closed)
-
-
-        Scaffold(
-
-
-        ) {
-
-        }
-
-
-
-
-
-            // Your main content goes here
-            NavHost(
-                navController = navController,
-                startDestination = Screen.HomeScreen.route
-            ) {
-                composable(Screen.HomeScreen.route) {
-                    HomeScreen()
-                }
-                composable(Screen.ProfileScreen.route) {
-                    ProfileScreen()
-                }
-                composable(Screen.SettingsScreen.route) {
-                    SettingsScreen()
-                }
-            }
-
+@Composable
+fun UrgentScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Text(
+            text = "Urgent Screen",
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
+}
 
-
-    @Composable
-    fun HomeScreen() {
-        // Your Home screen content goes here
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            Text(
-                text = "Home Screen",
-                modifier = Modifier.align(Alignment.Center),
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Text(
+            text = "Settings Screen",
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
-
-    @Composable
-    fun ProfileScreen() {
-        // Your Profile screen content goes here
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            Text(
-                text = "Profile Screen",
-                modifier = Modifier.align(Alignment.Center),
-                style = MaterialTheme.typography.bodyMedium
-
-            )
-        }
-    }
-
-    @Composable
-    fun SettingsScreen() {
-        // Your Settings screen content goes here
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            Text(
-                text = "Settings Screen",
-                modifier = Modifier.align(Alignment.Center),
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-    }
-
-
-
-
-
-
-
 }
